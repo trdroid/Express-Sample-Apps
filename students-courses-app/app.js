e2aba19ed448c7484d4a42fb5ba90b6e49e67195
gruprog@gmail.com <gruprog@gmail.com>
2016-04-24 14:32:23 -0400

    code changes and snapshot

--- students-courses-app/app.js
+++ students-courses-app/app.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/students', students)
+app.use('/students', students);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- students-courses-app/routes/students.js
+++ students-courses-app/routes/students.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var studentsModel= require('../models/students.js');
 
+// responds to /students
 router.get('/', function(req, res, next) {
 	res.json(studentsModel.getStudents());  
 });
